e5cddd9b28c392e91775f94d6bc1578c66c6576c
skhiearth <skhiearth@gmail.com>
2021-06-07 21:51:07 +0530

    PoS

--- Transaction Tracking/dataFetch.py
+++ Transaction Tracking/dataFetch.py
@@ -5,9 +5,9 @@ import csv
 # Etherscan API Setup
 URL = "https://api.etherscan.io"
 API_KEY = "9KDCGM8H25R414U4U5IVP4TEJQ91Y7AZC5"
-START_BLOCK = "12546134" # ~ 1st June 2021 08:47 AM IST
-END_BLOCK = "12548142" # ~ 1st June 2021 4:24 PM IST
-CONTRACT_ADDRESS = "0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf"
+START_BLOCK = "12540855" # ~ May-31-2021 07:43:05 AM +UTC
+END_BLOCK = "12588145" # ~ Jun-07-2021 03:15:58 PM +UTC
+CONTRACT_ADDRESS = "0x40ec5b33f54e0e8a33a975908c5ba1c14e5bbbdf"
 
 endpoint = "{}/api?module=account&action=tokentx&address={}&startblock={}&endblock={}&offset=10&sort=asc&apikey={}".format(URL ,CONTRACT_ADDRESS, START_BLOCK, END_BLOCK, API_KEY)
 
@@ -17,7 +17,7 @@ JSON_DATA_RAW = response.json()
 JSON_DATA = JSON_DATA_RAW['result']
 
 # Converting JSON data to CSV
-MATIC_BRIDGE = open('./maticbridge.csv', 'w')
+MATIC_BRIDGE = open('./posbridge.csv', 'w')
 csv_writer = csv.writer(MATIC_BRIDGE)
 
 count = 0
